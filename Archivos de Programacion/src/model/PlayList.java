fff32c5994279a074c7e0bbd12189af87cc2a853
Gabriel Suarez <gabriel.suarez1@correo.icesi.edu.co>
2020-10-18 16:09:08 -0500

    Version 1.1

--- Archivos de Programacion/src/model/PlayList.java
+++ Archivos de Programacion/src/model/PlayList.java
@@ -4,3 +4,22 @@ A00368589
 */
 
 package model;
+
+public class PlayList{
+
+  private String namePlayList;
+  private Duration durationPlayList;
+  private Gender genderPlayList;
+  private int typePlayList;
+
+  public PlayList(String namePlayList, Duration durationPlayList, Gender genderPlayList, int typePlayList){
+    this.namePlayList = namePlayList;
+    durationPlayList = 0;
+    gender = "";
+    typePlayList = 0;
+  }
+
+  public String getNamePlayList(){
+    return namePlayList;
+  }
+}
--- Archivos de Programacion/src/model/Song.java
+++ Archivos de Programacion/src/model/Song.java
@@ -6,5 +6,36 @@ A00368589
 package model;
 
 public class Song{
-  
+
+  private String tittleSong;
+  private String artistName;
+  private String realaseDateSong;
+  private Duration durationSong;
+  private Gender genderSong;
+  private int index;
+  private Gender[] allGender = Gender.values();
+
+  public Song(String tittleSong, String artistName, String realaseDateSong, Duration durationSong, int index){
+    this.tittleSong = tittleSong;
+    this.artistName = artistName;
+    this.realaseDateSong = realaseDateSong;
+    this.durationSong = durationSong;
+    genderSong = allGender[index-1];
+  }
+
+  public String getTittleSong(){
+    return tittleSong;
+  }
+
+  public String getArtistName(){
+    return artistName;
+  }
+
+  public String getRealaseDateSong(){
+    return realaseDateSong;
+  }
+
+  public Duration getDurationSong(){
+    return durationSong;
+  }
 }
